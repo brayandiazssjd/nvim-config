ac0a750b07351d927f6ee43bff08a2b85141e1f3
brayandiazssjd <brayandiazssjd@gmail.com>
2025-12-02 20:38:40 -0500

    Added Telescope keymaps in Which-key

--- lua/whichkey.lua
+++ lua/whichkey.lua
@@ -2,7 +2,13 @@ local wk = require("which-key")
 
 local mappings = {
   { "<leader>f", group = "file" }, -- group
-  { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find File", mode = "n" },
+  { "<leader>ff", "<cmd>Telescope find_files<cr>", desc="Find File", mode="n" },
+  { "<leader>fb", "<cmd>Telescope buffers<cr>", desc="Buffers", mode="n" },
+  { "<leader>fl", "<cmd>Telescope live_grep", desc="Live grep", mode="n" },
+  { "<leader>fs", "<cmd>Telescope grep_string", desc="Grep string", mode="n" },
+  { "<leader>fh", "<cmd>Telescope help_tags", desc="Help tags", mode="n" },
+  { "<leader>fq", "<cmd>Telescope quickfix", desc="Quick fix", mode="n" },
+  { "<leader>fy", "<cmd>Telescope lsp_document_symbols", desc="Doc symbols", mode="n" },
   { "<leader>e", "<cmd>NvimTreeToggle<cr>", desc = "Exprorer" },
   { "<leader>f1", hidden = true }, -- hide this keymap
   { "<leader>d", "<cmd>lua vim.diagnostic.open_float()<cr>", desc = "Diagnostic"},
